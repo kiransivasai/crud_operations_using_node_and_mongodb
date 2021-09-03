19093ac375db19d7c4e3c86f70bc653005f3cad5
kiransivasai <kiransiva2060@gmail.com>
2021-09-03 16:15:05 +0530

    Added CRUD Operations

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Item = require("./models/items");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+const mongodb =
+  "mongodb+srv://siva:sai@cluster0.rkis5.mongodb.net/item-database?retryWrites=true&w=majority";
+mongoose
+  .connect(mongodb)
+  .then(() => {
+    console.log("Connected");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.redirect("/get-items");
+});
+
+app.get("/get-items", (req, res) => {
+  Item.find()
+    .then((result) => {
+      res.render("index", { items: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/get-item/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then((result) => {
+      res.render("item-detail", { item: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/add-item", (req, res) => {
+  res.render("add-item");
+});
+
+app.post("/items", (req, res) => {
+  const item = Item(req.body);
+  item
+    .save()
+    .then(() => {
+      res.redirect("/get-items");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.delete("/items/:id", (req, res) => {
+  Item.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.json({ redirect: "/get-items" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.put("/items/:id", (req, res) => {
+  const id = req.params.id;
+  Item.findByIdAndUpdate(id, req.body).then((result) => {
+    res.json({ msg: "Updated Successfully" });
+  });
+});
+
+app.use((req, res) => {
+  res.render("error");
+});
